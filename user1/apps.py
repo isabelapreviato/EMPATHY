408fbd262461bc6867b5c9cd759cb2e37034722f
isabelapreviato <isabelapreviato@hotmail.com>
2019-03-14 20:59:22 -0300

    remoção de app'user1' app 'cadastro'

--- user1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class User1Config(AppConfig):
-    name = 'user1'
--- user1/views.py
+++
@@ -1,6 +0,0 @@
-# from django.shortcuts import render
-
-# # Create your views here.
-
-# def index1(request):
-#     return render(request, 'layout.html', {})
