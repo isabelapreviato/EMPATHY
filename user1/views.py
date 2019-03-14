e398401322ccc28105854916a50aad3bedd61c82
isabelapreviato <isabelapreviato@hotmail.com>
2019-03-14 20:29:18 -0300

    foi 'retirado' app user1

--- user1/views.py
+++ user1/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
 
-def index(request):
-    return render(request, 'layout.html', {})
+# def index1(request):
+#     return render(request, 'layout.html', {})
